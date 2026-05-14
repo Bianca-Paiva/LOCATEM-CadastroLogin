5dbc7a35b32b045c5ebb9a400bf5db0c09cc9e84
Stephany <tete.calmon@hotmail.com>
2026-05-14 16:41:36 -0300

    ligação tela login

--- src/js/login.js
+++ src/js/login.js
@@ -17,3 +17,59 @@ eyeBtn.addEventListener("click", () => {
     // (ex: mostrar olho aberto ou fechado)
     eyeBtn.classList.toggle("active");
 });
+
+// LOGIN para 
+
+const form = document.getElementById("formLogin");
+
+form.addEventListener("submit", async (event) => {
+
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+
+    const senha = document.getElementById("senha").value;
+
+    try {
+
+        const resposta = await fetch(
+            "https://localhost:7127/api/auth/login",
+            {
+                method: "POST",
+
+                headers: {
+                    "Content-Type": "application/json"
+                },
+
+                body: JSON.stringify({
+                    email: email,
+                    senha: senha
+                })
+            }
+        );
+
+        const dados = await resposta.json();
+
+        console.log(dados);
+
+        if (resposta.ok) {
+
+            localStorage.setItem("token", dados.token);
+
+            alert("Login realizado com sucesso!");
+
+            window.location.href = "./principalPage.html";
+        }
+        else {
+
+            alert("Email ou senha inválidos");
+        }
+
+    }
+    catch (erro) {
+
+        console.error(erro);
+
+        alert("Erro ao conectar com a API");
+    }
+});
